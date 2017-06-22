74adb28a551d97829e4ec1d20ff5a6ddab778165
akshay58165 <typeakshay@gmail.com>
2017-06-22 21:32:55 +0530

    Update python-oop.py

--- python-oop.py
+++ python-oop.py
@@ -1,6 +1,27 @@
-class Car:
-	def __init__(self, model, make):
-		self.model = model
-		self.make = make
+# class example
+class A:
+	def f(self):
+		return self.g()
+		
+	def g(self):
+		return 'A'
+			
+class B(A):
+	def g(self):
+		return 'B'
 
-
+# Repl:
+   a = A()
+   a
+=> <A object at 0x7fdc9d7d6f60>
+   b = B()
+   b
+=> <B object at 0x7fdc9d7dae10>
+   a.f
+=> <bound method A.f of <A object at 0x7fdc9d7d6f60>>
+   b.f
+=> <bound method A.f of <B object at 0x7fdc9d7dae10>>
+   a.g
+=> <bound method A.g of <A object at 0x7fdc9d7d6f60>>
+   b.g
+=> <bound method B.g of <B object at 0x7fdc9d7dae10>>
